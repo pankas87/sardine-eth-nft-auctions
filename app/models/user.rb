eb4b25f22b578bcadad3b190c460434528a738aa
Leonardo Graterol <pankas87@gmail.com>
2022-01-31 18:16:04 -0300

    User model, additional tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,18 @@
 class User < ApplicationRecord
   validates :username, uniqueness: true
+  validates :address, presence: true
+  validates :secret_key, presence: true
+
+  def before_save
+=begin
+    - Secret_Key:
+      - Automatically generate (before_save hook)
+    - SHA-256 of:
+      - Current timestamp
+    - Configurable salt value
+    - Random number
+    - Should be unique as well
+    - Optional: SHA256 hashed
+=end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,15 +3,21 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   # Basic Validations
   it { should validate_uniqueness_of(:username) }
+  it { should validate_presence_of(:address) }
+  it { should validate_presence_of(:secret_key)}
 
+  # TODO: Optional, depending on time
+  it { pending "validate correct format of ETH wallet address, might be tricky since wallet addresses could have different formats" }
+  it { pending "Validate presence of association with bids, once the bid models exist" }
+
+  # TODO: Before block, mock the time.now or whatever function I use to get the timestamp for generating the secret key
   describe "secret key tests" do
-    # TODO: Before block, mock the time.now or whatever function I use to get the timestamp for generating the secret key
     it "generates a secret key for new users" do
-
+      pending "this needs to be implemented"
     end
 
     it "does not generate a secret key for existing users being updated" do
-
+      pending "this needs to be implemented"
     end
   end
 end
