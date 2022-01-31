eb3d1b085ce6b3c80cb26d973e48586380b828fa
Leonardo Graterol <pankas87@gmail.com>
2022-01-31 18:10:35 -0300

    User models, first tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe "It applies all the pertinent validations" do
+  # Basic Validations
+  it { should validate_uniqueness_of(:username) }
 
+  describe "secret key tests" do
+    # TODO: Before block, mock the time.now or whatever function I use to get the timestamp for generating the secret key
+    it "generates a secret key for new users" do
+
+    end
+
+    it "does not generate a secret key for existing users being updated" do
+
+    end
   end
 end
