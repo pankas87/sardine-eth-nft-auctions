d7654e6a1dc0b871af7e42925b0d8af55cda3b72
Leonardo Graterol <pankas87@gmail.com>
2022-02-01 23:15:43 -0300

    Refactoring BidsController

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -1,25 +1,14 @@
 class BidsController < ApplicationController
+  before_action :get_secret_key, :authenticate_user
+
   def create
-    secret_key = request.headers["Auth"]
+    @bid = Bid.new bid_params
+    @bid.user = @user
 
-    # TODO: This nesting of ifs is ugly AF, refactor after having tests in place, use before_action
-    if secret_key.nil?
-      render json: {error: "Auth header is not present"}, status: :unauthorized
+    if @bid.save
+      render json: {}, status: :created
     else
-      user = User.find_by(secret_key: secret_key)
-
-      if user.nil?
-        render json: {error: "Secret Key does not correspond to an existing user"}, status: :not_found
-      else
-        @bid = Bid.new bid_params
-        @bid.user = user
-
-        if @bid.save
-          render json: {}, status: :created
-        else
-          render json: { errors: @bid.errors }, status: :unprocessable_entity
-        end
-      end
+      render json: { errors: @bid.errors }, status: :unprocessable_entity
     end
   end
 
@@ -27,5 +16,21 @@ class BidsController < ApplicationController
 
   def bid_params
     params.permit(:amount)
+  end
+
+  def get_secret_key
+    @secret_key = request.headers["Auth"]
+
+    if @secret_key.nil?
+      render json: {error: "Auth header is not present"}, status: :unauthorized
+    end
+  end
+
+  def authenticate_user
+    @user = User.find_by(secret_key: @secret_key)
+
+    if @user.nil?
+      render json: {error: "Secret Key does not correspond to an existing user"}, status: :not_found
+    end
   end
 end
